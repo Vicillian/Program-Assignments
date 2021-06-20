ae5a2cd6df781868ee73802e0a9626c292c9fa81
Vicillian <vic.liu101@gmail.com>
2021-06-19 21:38:37 -0700

    Lab 2 Finished

--- Dictionary_Practice.py
+++ Dictionary_Practice.py
@@ -1,5 +1,8 @@
 dict_food = {"chicken": 1.64, "beef": 1.99, "milk": 2.50,}
 
 chicken_price = dict_food["chicken"]
-print("Price of chicken =" + chicken_price)
+print("Price of chicken:" + chicken_price)
 
+dict_food["beef"] = 2
+beef_price = dict_food["beef"]
+print("Price of beef:" + beef_price)
---
+++ list_practice.py
@@ -0,0 +1,9 @@
+empty_list = []
+city_list = ["Oakland", "Atlanta", "New York City", "Seattle", "Mempish", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+##print(city_list)
+
+bean_list = ["Pinto Bean", "String Bean", "Green bean", "Refried Bean", "Raw Bean", "Mr.Bean"]
+##print(bean_list)
+
+new_list= ["new 1", "new 2 "]
+type(new_list)
